472de71dbf0da10e94a9a3a917462c075abb56e5
David M <david.myklebust@gmail.com>
2013-02-03 23:58:09 +0100

    Added shape.cpp, .h

---
+++ shape.cpp
@@ -0,0 +1,14 @@
+
+#include <shape.h>
+
+//
+//	Shape class
+//
+
+float Shape::area() {
+	return 0;
+}
+
+float Shape::circumferance() {
+	return 0;
+}
---
+++ shape.h
@@ -0,0 +1,13 @@
+
+//
+//	Shape class
+//
+
+class Shape {
+
+public:
+
+	virtual float area();
+	virtual float circumferance();
+	
+};
